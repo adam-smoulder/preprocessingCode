4b704e786c5b4961113f59b8320c27d0b010c149
Adam Smoulder <asmoulder3195@gmail.com>
2022-10-27 15:29:49 -0400

    Spike alignment
    Added spike alignment to trial data

--- README.md
+++ README.md
@@ -7,13 +7,17 @@ Simple task and kinematic processing:
 
 wrapper_preprocessTaskAndKinematicData_choking is a script where you can pass in lists of saved .mat files with synchronized data and folders with task data and get out processed task data. It calls pp_preprocessTaskAndKinematicData_choking, which processes the raw task data to extract relevant info (i.e., smoothed/upsampled kinematics, trial labels for target direction and reward, etc.). For tasks with parameters beyond reward and target (i.e., target size tasks, focus task), pp_preprocessTaskAndKinematicData_choking will probably need to be expanded copied -> adapted.
 
+Spike alignment to trial data
+
+wrapper_initalProcessTaskKinematicSpikeData_choking is a script that does both the initial behavioral processing mentioned above, and also runs pp_spikeAlignmentToTrial, which updates taskInfo by adding on a matrix of [nunits x nsnippetTimepoints] waveform means and updates trialData to have the neuralData field. neuralData contains the channel number and sort number for each unit, an a [nunits x ntime] boolean spikeMatrix for each trial, indicating at millisecond precision if a spike was observed at that time or not.
+
+
 # Other things to implement and include here
 
 Other things to go here:
 - Analog data processing
   - EMG, EKG, eye - probably align to trial, normalize if needed (i.e. pupil, EMG)
 - Neural data processing
-  - initial processing first, like alignment
-  - further processing later, like artifact detection, unit IDing across days, and stitching preprocessing
+  - further processing, like artifact detection, unit IDing across days, and stitching preprocessing
 
 Overall, stuff will probably fall into 3 steps: (1) initial processing to get raw data in a usable form, (2) early preprocessing that extracts things relevant for all tasks from this data (i.e., behavioral metrics, unit identification, general artifact removal, maybe stitching?), (3) more specific experiment-specific processing and analysis
---
+++ wrapper_initalProcessTaskKinematicSpikeData_choking.m
@@ -0,0 +1,85 @@
+%%% This script combines synchronized analog data files with task data and
+%%% processes the task data into a usable form (i.e., smooths/interpolates
+%%% kinematics, extracts trial labels, etc.). It then adds neural activity
+%%% timestamp matrices to the trials as well.
+%%%
+%%% Adam Smoulder, 10/27/22
+
+dbstop if error
+
+% Edit folder/filenames and file tags (what they start with) here
+spikeAndAnalogDataFileList = {...
+    'F:\~~~RockyData\BORTRig\~alignedSpikeAndAnalogData_synchDrops\Rocky_synchedSpikeAndAnalogData_noSPM_20220302.mat';
+    };
+taskDataFolderList = {...
+    'F:\~~~RockyData\BORTRig\TaskData\20220216to20220303_chokingReachingExperimentsAndChoice\centerOutData_20220302_153642\';
+    };
+workingFolder = [pwd '\']; % This is where the functions and classes needed are, at least beneath it
+outputFolder = [workingFolder 'preprocFiles_behaviorAndSpikeInitialProcessed\\'] ; % where to save files
+
+% Things that shouldn't change over days
+trialDataTag = 'Trial0'; % what each trial data file starts with
+rewardNames = {'Small','Medium','Large','Jackpot'}; % Searches target names for these
+
+% Quickly check to make sure numbers of files all line up
+nfiles = length(spikeAndAnalogDataFileList);
+assert(length(taskDataFolderList)==nfiles);
+
+% Go to working folder
+cd(workingFolder) 
+
+% % Adam uses this to remove paths to other stuff
+% rmpath(genpath('D:\AdamMatlab'))
+% addpath(genpath(pwd))
+
+
+%% For each day, preprocess the data and save a file
+
+for f = 1:nfiles
+    dateString = taskDataFolderList{f}(end-15:end-1) % show it for confirmation
+    load([taskDataFolderList{f} 'taskInfo.mat'])
+    
+    % Process the behavior
+    load(spikeAndAnalogDataFileList{f})
+    [trialData,taskInfo] = pp_preprocessTaskAndKinematicData_choking(taskDataFolderList{f},rewardNames,synchInfo);
+    
+    % Attach spikes to trialData
+    [trialData,taskInfo] = pp_spikeAlignmentToTrial(taskAlignedSpiketimes,synchInfo,trialData,taskInfo);
+    
+    % Save!
+    saveFilename = ['Rocky' ...
+        taskDataFolderList{f}(end-15:end-8) ...
+        '_Trials' num2str(trialData(1).trial) ...
+        '_' num2str(trialData(end).trial) '_behaviorAndSpikesInitialProcessed_' ...
+        grabDateTimeString];
+    save([outputFolder saveFilename],'trialData','taskInfo','analogData','synchInfo','taskAlignedSpiketimes')
+    
+%     % If these files get too big, split up into smaller files
+%     disp('Saving in sections of 500 trials')
+%     allData = data; clear data
+%     nsections = ceil(length(allData)/500);
+%     for s = 1:nsections
+%         if s == nsections
+%             data = allData((s-1)*500+1:end);
+%         else
+%             data = allData((s-1)*500+1:s*500);
+%         end
+%         saveFilename = [data(1).Overview.subjectName taskName ...
+%         num2str(floor(data(1).Overview.date/1E6)) ...
+%         '_Trials' data(1).Overview.trialNumber(6:end) ...
+%         '_' data(end).Overview.trialNumber(6:end) '_behaviorAndSpikesInitialProcessed_' ...
+%         grabDateTimeString]
+%         save([saveFolder saveFilename],'data','taskInfo')
+%         clear data
+%     end; clear s
+    
+    disp('Saved!')
+    disp(['Completed preprocessing for file ' num2str(f) ' of ' num2str(nfiles)])
+    
+    clear trialData analogData synchInfo taskAlignedSpiketimes taskInfo
+end; clear f
+
+
+
+
+
